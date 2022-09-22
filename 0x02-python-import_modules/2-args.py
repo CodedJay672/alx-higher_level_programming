b503c06fe4a3bc37e0812bb91c3ad41acfdac0ee
Codedjay672 <emmajoel6456@gmail.com>
2022-09-22 23:13:05 +0000

    update module scripts

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,11 @@ import sys
 if __name__ == "__main__":
     i = len(sys.argv)
     if i == 1:
-        print("{:d} argument.".format(i - 1))
+        print("{:d} arguments.".format(i - 1))
+    elif i == 2:
+        print("{:d} argument:".format(i - 1))
+        for i in range(1, i):
+            print("{}: {}".format(i, sys.argv[i]))
     else:
         print("{:d} arguments:".format(i - 1))
         for i in range(1, i):
