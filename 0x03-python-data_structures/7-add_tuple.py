b77eb772bc9e78d8043d36edb191ade1e1c98272
Codedjay672 <emmajoel6456@gmail.com>
2022-09-27 15:47:19 +0000

    update data structures

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -5,14 +5,12 @@ def add_tuple(tuple_a=(), tuple_b=()):
     result = []
     new_tuple_a = list(tuple_a)
     new_tuple_b = list(tuple_b)
-    if len(new_tuple_a) == 0:
-        return tuple_b
-    if len(new_tuple_b) == 0:
-        return tuple_a
-    if len(new_tuple_a) < 2:
-        new_tuple_a.append(0)
-    if len(tuple_b) < 2:
-        new_tuple_b.append(0)
     for i in range(2):
+        if len(new_tuple_a) < 2:
+            new_tuple_a.append(0)
+    for j in range(2):
+        if len(tuple_b) < 2:
+            new_tuple_b.append(0)
+    for k in range(2):
         result.append(new_tuple_a[i] + new_tuple_b[i])
     return tuple(result)
