0b9e28550d277d6fd01ea97838066ce04d5a76d4
Codedjay672 <emmajoel6456@gmail.com>
2022-09-27 14:41:09 +0000

    update data structure

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     new_list = []
-    if my_list == None or len(my_list) == 0:
+    if my_list is None or len(my_list) == 0:
         return None
     for i in my_list:
         if i % 2 == 0:
