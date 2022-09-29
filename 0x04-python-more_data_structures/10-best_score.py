5a43db6d98cff411f3df021227d9f2f912c10c4e
Codedjay672 <emmajoel6456@gmail.com>
2022-09-29 04:33:09 +0000

    update data structures

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    highscore = 0
-    name = ""
     if a_dictionary is None:
         return None
+    scores = list(a_dictionary.values()) 
+    highscore = scores[0]
+    name = ""
     for key, value in a_dictionary.items():
         if a_dictionary[key] > highscore:
             highscore = value
