5bcc3a7d657fb97fa629aa1b34859c5766f454ef
Codedjay672 <emmajoel6456@gmail.com>
2022-10-12 20:03:17 +0000

    update exceptions

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import sys
+def safe_function(fct, *args):
+    try:
+        return fct(*args)
+    except (ValueError, ZeroDivisionError, NameError, IndexError) as ex:
+        print("Exception: {}".format(ex), file=sys.stderr)
+        return None
