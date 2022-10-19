fb5526dd399f23c6a4667df7b1e41f8a699dbdde
Codedjay672 <emmajoel6456@gmail.com>
2022-10-19 16:29:00 +0000

    update rec class

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -15,17 +15,16 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """constructor to initialize width and height"""
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     def width(self, value):
         """ set width value """
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
-        else:
-            self.__width = value
+        self.__width = value
 
     def width(self):
         """ get width value """
@@ -39,7 +38,6 @@ class Rectangle:
         """ set height value """
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
-        else:
-            self.__height = value
+        self.__height = value
