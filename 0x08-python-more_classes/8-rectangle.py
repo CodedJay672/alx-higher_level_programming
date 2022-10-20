e3c5263124c415d754d9207a60db1272c8038fc1
Codedjay672 <emmajoel6456@gmail.com>
2022-10-20 02:06:18 +0000

    wrong commit

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -88,18 +88,3 @@ class Rectangle:
 
         print("Bye rectangle...")
         type(self).number_of_instances -= 1
-
-    @staticmethod
-    def bigger_or_equal(rect_1, rect_2):
-        """
-        compares two rectangle by area and returns the largest.
-        arguments must be of Rectangle type else an exception is raised
-
-        """
-        if not isinstance(rect_1, Rectangle):
-            raise TypeError("rect_1 must be an instance of Rectangle")
-        if not isinstance(rect_2, Rectangle):
-            raise TypeError("rect_2 must be an instance of Rectangle")
-        if rect_1.area() >= rect_2.area():
-            return rect_1
-        return rect_2
