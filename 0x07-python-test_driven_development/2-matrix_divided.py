8ee9be2034ab73bdad3d35f6279b21b615be7d6a
Codedjay672 <emmajoel6456@gmail.com>
2022-10-21 21:54:36 +0000

    update 2-matrix_divided.py

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 
 """ function that divides all elements of a matrix """
+
+
 def matrix_divided(matrix, div):
-    """ 
+    """
     matrix divider returns a new divided matrix
     new matrix works with integers and floats.
 
@@ -13,16 +15,19 @@ def matrix_divided(matrix, div):
     if div == 0:
         raise ZeroDivisionError("division by zero")
     if not isinstance(matrix, list):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError("matrix must be a matrix \
+                        (list of lists) of integers/floats")
 
     size = len(matrix[0])
 
     for mat_list in matrix:
         if not isinstance(mat_list, list):
-            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            raise TypeError("matrix must be a matrix \
+                            (list of lists) of integers/floats")
         if len(mat_list) != size:
             raise TypeError("Each row of the matrix must have the same size")
-        for l in mat_list:
+        for list_element in mat_list:
             if (not isinstance(l, int) and not isinstance(l, float)):
-                raise TypeError("matrix must be a  matrix (list of lists) of integers/floats")
+                raise TypeError("matrix must be a  matrix \
+                                (list of lists) of integers/floats")
     return [[round(j / div, 2) for j in my_list] for my_list in matrix]
