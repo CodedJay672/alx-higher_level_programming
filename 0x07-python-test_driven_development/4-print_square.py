2c9af9ad312c9b68333f5a995734e105b783e46c
Codedjay672 <emmajoel6456@gmail.com>
2022-10-22 20:28:12 +0000

    updating the test file

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -6,6 +6,7 @@ function argument is the 'size' of the square
 
 """
 
+
 def print_square(size):
     """
     function shich prints a square on stdout
@@ -14,12 +15,12 @@ def print_square(size):
     """
     char = "#"
 
-    if type(size) is float and size < 0:
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if (type(size) is float and size < 0):
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
-    if not isinstance(size, int):
-        raise TypeError("size must be an integer")
     for row in range(size):
         for cols in range(size):
             print(char, end="")
