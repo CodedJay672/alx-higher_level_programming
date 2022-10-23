016e7da47dcf732a460e65b34f5b47ac5d4b2871
Codedjay672 <emmajoel6456@gmail.com>
2022-10-23 13:33:21 +0000

    update 5-text_indentation

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+"""
+function prints a text with 2 lines after ., ? and :
+argument is a text containing many characters
+
+"""
+
+def text_indentation(text):
+    """
+    prints 2 lines after ., ? and :
+    takes a text as argument
+
+    """
+
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    count = 0
+
+    while count < len(text):
+        print("{}".format(text[count]), end="")
+        if (text[count] == "." or text[count] == "?" or text[count] == ":"):
+            print("\n")
+            count += 1
+        count += 1
