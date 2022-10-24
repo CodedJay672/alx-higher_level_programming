57ea414de9ce68690a7a610b52c89eba4d58ea63
Codedjay672 <emmajoel6456@gmail.com>
2022-10-24 00:56:31 +0000

    update integer tests

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Unittest for max_integer([..])
+
+"""
+import unittest
+
+
+max_integer = __import__('6-max_integer').max_integer
+
+class TestMaxInteger(unittest.TestCase):
+    """test cases"""
+    def test_emptyArgs(self):
+        self.assertFalse(max_integer(), None)
+
+    def test_oneArg(self):
+        self.assertTrue(max_integer([4]), 4)
+
+    def test_maxLast(self):
+        self.assertTrue(max_integer([1, 2, 3, 4]), 4)
+
+    def test_maxFirst(self):
+        self.assertTrue(max_integer([4, 3, 2, 1]), 4)
+
+    def test_emptyList(self):
+        self.assertFalse(max_integer([]), None)
