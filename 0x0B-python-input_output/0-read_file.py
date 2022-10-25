e3970544857caa197fed7669101a4e6ab96db6be
Codedjay672 <emmajoel6456@gmail.com>
2022-10-25 10:18:13 +0000

    update 0-read_file.py

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""module that reads a file and prints to stdout"""
+
+def read_file(filename=""):
+    """function which reads a file and writes to stdout"""
+
+    with open(filename, "r+", encoding="utf-8") as f:
+        read_data = f.read()
+        s = str(read_data)
+        print(s)
