3122e54d6b87af0a036742a0aa1ce16c794fea3f
Codedjay672 <emmajoel6456@gmail.com>
2022-10-26 10:52:48 +0000

    update 8-class_to_json.py

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Class to json module"""
+
+def class_to_json(obj):
+    """function which converts python object to json dict
+    Args:
+        @obj: object instance of a class
+    Returns a dict representation of the object
+    """
+
+    return obj.__dict__
