2796d58b65236690bb9ffbc4d410801f3e1af09f
Codedjay672 <emmajoel6456@gmail.com>
2022-11-01 03:33:39 +0000

    update base module

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -34,3 +34,35 @@ class Base:
             else:
                 list_dicts = [out.to_dictionary() for out in list_objs]
                 f.write(cls.to_json_string(list_dicts))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """funct to convert from a json string"""
+        if json_string is None:
+            return {}
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """Return a class instantiated from a dictionary of attributes.
+        Args:
+            **dictionary (dict): Key/value pairs of attributes to initialize.
+        """
+        if dictionary and dictionary != {}:
+            if cls.__name__ == "Rectangle":
+                new = cls(1, 1)
+            else:
+                new = cls(1)
+            new.update(**dictionary)
+            return new
+
+    @classmethod
+    def load_from_file(cls):
+        """funct to load from file"""
+        filename = cls.__name__ + ".json"
+        try:
+            with open(filename, 'r') as f:
+                list_obj = cls.from_json_string(f.read())
+                return [cls.create(**d) for d in list_obj]
+        except IOError:
+            return []
