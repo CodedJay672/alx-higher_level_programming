5e8a2e404aa3fef725d8b327fc977efb94a47085
Codedjay672 <emmajoel6456@gmail.com>
2022-11-30 15:51:26 +0000

    update sql insert statement

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,3 @@
+-- Script which inserts  new row into a table in the database
+-- INSERT statement allows us to insert rows and their values
+INSERT INTO first_table (id, name) VALUES (89, 'Best School')
