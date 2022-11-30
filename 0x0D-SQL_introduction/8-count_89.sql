d13c7e8b7e930bcc67e2e022745ed108d5ba30e6
Codedjay672 <emmajoel6456@gmail.com>
2022-11-30 16:31:59 +0000

    count script update

---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,3 @@
+-- Script to display number of records with id = 89 in the first_table
+-- COUNT() statement can be used. We implement the WHERE as well
+SELECT COUNT(*) FROM first_table WHERE id = 89
