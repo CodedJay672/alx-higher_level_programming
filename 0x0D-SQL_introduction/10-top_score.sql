e32120864fb424ab24fe7a11a248eb5cec394a1f
Codedjay672 <emmajoel6456@gmail.com>
2022-11-30 17:22:08 +0000

    update select...order by query

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,3 @@
+-- script that selects score and name from second table
+-- INSERT...FROM statement with the GROUP BY option can solve this problem
+SELECT score, name FROM second_table ORDER BY score DESC;
