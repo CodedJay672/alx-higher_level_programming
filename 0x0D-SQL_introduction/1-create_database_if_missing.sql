a8abf2f5940987a88f18de9a68f64d13387d6dda
Codedjay672 <emmajoel6456@gmail.com>
2022-11-30 03:37:17 +0000

    update optional database create

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,3 +1,3 @@
 -- script which creates a database if it doesnt exist
 -- SQL query which creates a database with the "IF NOT EXIST" statement
-CREATE DATABASE [IF NOT EXISTS] hbtn_0c_0
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0
