2fca43f94cf257e227ec97a8d18640da4a017767
Codedjay672 <emmajoel6456@gmail.com>
2022-12-02 10:40:51 +0000

    update 15-groups.sql

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,3 +1,6 @@
 -- script which counts rows with same scores
 -- SELECT statement with the count() function is used
-SELECT score, count(score) AS number FROM second_table GROUP BY score;
+SELECT score, count(score) AS number
+	FROM second_table
+	GROUP BY score
+	ORDER BY count(score) DESC;
