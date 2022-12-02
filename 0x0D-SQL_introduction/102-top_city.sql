1b6d8588b3ce87bfdcd58b50294b29c234437083
Codedjay672 <emmajoel6456@gmail.com>
2022-12-02 16:06:19 +0000

    update limit script

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,7 @@
+-- script which displays the top THREE cities by average temperature
+-- SELECT the cities and grouping them can solve this problem
+SELECT city, avg(value) FROM temperatures
+	WHERE month = 7 OR month = 8
+	GROUP BY city
+	ORDER BY AVG(value) DESC
+	LIMIT 3;
