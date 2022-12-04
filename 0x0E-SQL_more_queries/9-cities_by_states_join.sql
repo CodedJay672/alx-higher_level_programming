14090d8d93cf085f4e7cc6a090e24fab052207ff
e_Manny <55061323+CodedJay672@users.noreply.github.com>
2022-12-04 03:11:19 +0100

    Delete 9-cities_by_states_join.sql

--- 0x0E-SQL_more_queries/9-cities_by_states_join.sql
+++
@@ -1,6 +0,0 @@
--- script that displays cities by their states
--- We have to join tables cities and states to solve this problem
-SELECT c.id, c.name, s.name
-FROM cities c JOIN states s
-	ON c.id = s.id
-ORDER BY c.id ASC;
