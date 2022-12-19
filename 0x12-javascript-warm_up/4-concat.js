b4bb7d22b708c5b2795d2415bace21d7131ed6a7
codedjay672 <emmajoel672@gmail.com>
2022-12-19 17:48:35 +0000

    update concate file

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,9 +1,3 @@
 #!/usr/bin/node
 const args = process.argv;
-if (args.length === 3) {
-  console.log(args[2] + ' is ' + args[3]);
-} else if (args.length === 4) {
-  console.log(args[2] + ' is ' + args[3]);
-} else {
-  console.log(args[2] + ' is ' + args[3]);
-}
+console.log(args[2] + ' is ' + args[3]);
