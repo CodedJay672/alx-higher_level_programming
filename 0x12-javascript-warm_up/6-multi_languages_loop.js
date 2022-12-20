f048d0f419e0be2bf088b45e8274290d9cd57e9d
codedjay672 <emmajoel672@gmail.com>
2022-12-20 08:59:20 +0000

    update array

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const sentences = ['C is fun',
   'Python is cool',
-  'Javascript is amazing'];
+  'JavaScript is amazing'];
 let i = 0;
 while (i < sentences.length) {
   console.log(sentences[i]);
