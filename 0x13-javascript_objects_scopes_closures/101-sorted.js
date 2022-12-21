7c6c8ba9669b4372bac686c1763e50493dd9ed56
codedjay672 <emmajoel672@gmail.com>
2022-12-21 00:22:26 +0000

    update sorted list

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const dict = require('./101-data.js').dict;
+
+const dictEntries = Object.entries(dict);
+const dictVals = Object.values(dict);
+const uniqVals = [...new Set(dictVals)];
+const newDict = {};
+
+for (const j in uniqVals) {
+  const list = [];
+  for (const k in dictEntries) {
+    if (dictEntries[k][1] === uniqVals) list.unshift(dictEntries[k][0]);
+  }
+  newDict[uniqVals[j]] = list;
+}
+console.log(newDict);
+
