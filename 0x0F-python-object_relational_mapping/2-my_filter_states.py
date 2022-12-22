44128cd616b562c72c7250e6e548dabe8b90b920
codedjay672 <emmajoel672@gmail.com>
2022-12-22 12:49:03 +0000

    update formatted sql query

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+"""
+   python script which searches for a name
+   in the states table.
+   The name of the state to search will be taken as
+   argument from the command line
+   script implements MySQLdb library
+
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    args = sys.argv
+    conn = MySQLdb.connect(
+            host="localhost",
+            user=args[1],
+            passwd=args[2],
+            db=args[3],
+            )
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states\
+                WHERE states.name = '{}'\
+                ORDER BY states.id;".format(sys.argv[4]))
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
