a17c0ddd17661ccf351aa7b943016d09cc65ee93
codedjay672 <emmajoel672@gmail.com>
2022-12-22 23:03:15 +0000

    first ORM update

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""
+   module which creates a class definition of a state object
+   Base class is included. Finally, links to MySQL table
+   "states"
+
+"""
+
+import sqlalchemy
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class State(Base):
+    __tablename__ = 'states'
+    id = Column(Integer(11),unique=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
