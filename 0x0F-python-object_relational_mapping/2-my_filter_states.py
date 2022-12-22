9054aaaffc2cb2df05af330ca3c9d316ad1b3eee
codedjay672 <emmajoel672@gmail.com>
2022-12-22 12:54:56 +0000

    update formatted query

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -23,7 +23,7 @@ if __name__ == "__main__":
     cur = conn.cursor()
     cur.execute("SELECT * FROM states\
                 WHERE states.name = '{}'\
-                ORDER BY states.id;".format(sys.argv[4]))
+                ORDER BY states.id ASC;".format(sys.argv[4]))
     rows = cur.fetchall()
 
     for row in rows:
