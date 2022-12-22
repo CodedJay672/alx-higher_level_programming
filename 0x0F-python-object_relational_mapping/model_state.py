67a110f4be7f87afeca6af50a4478df9908a3872
codedjay672 <emmajoel672@gmail.com>
2022-12-22 23:12:10 +0000

    update ORM class

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -7,7 +7,6 @@
 
 """
 
-import sqlalchemy
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -15,6 +14,14 @@ Base = declarative_base()
 
 
 class State(Base):
+    """
+        State class inherits from declarative Base class
+        class attributes:
+            @id: state id
+            @name: state name
+
+    """
+
     __tablename__ = 'states'
     id = Column(Integer(11), unique=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
