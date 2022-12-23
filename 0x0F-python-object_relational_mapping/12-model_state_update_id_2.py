84015952ec3180a1ff21ff23dd677785b5620302
codedjay672 <emmajoel672@gmail.com>
2022-12-23 04:49:11 +0000

    update state table

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""
+    Script that creates a new state
+
+"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == '__main__':
+    args = sys.argv
+    Engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(args[1], args[2], args[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=Engine)
+    session = Session()
+
+    state = session.query(State).filter(State.id == 2).first()
+    state.name = 'New Mexico'
+    session.add(state)
+    session.commit()
