54d8b2eb5dc6914df83f2abd8ba1511820e9055b
codedjay672 <emmajoel672@gmail.com>
2022-12-23 04:17:44 +0000

    update ORM

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+"""
+    script which fetches all state from a database
+    script args:
+        @host: localhost
+        @u.name: root
+        @passwd: root
+
+"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+    rows = session.query(State).filter(
+                                       State.name == sys.argv[4]
+                                       ).first()
+    if rows:
+        print('{}'.format(rows.id))
+    else:
+        print("Not Found")
