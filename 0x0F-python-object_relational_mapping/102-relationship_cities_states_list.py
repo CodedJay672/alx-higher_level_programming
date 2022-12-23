e3bbdba0f16f86ee58c421800779bf785dfa5b3b
codedjay672 <emmajoel672@gmail.com>
2022-12-23 18:52:31 +0000

    update cities with states

---
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+"""
+    Script that lists all states and their correspondin
+    cities.
+
+"""
+
+import sys
+from sqlalchemy import create_engine
+from relationship_state import Base, State
+from relationship_city import City
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == '__main__':
+    args = sys.argv
+    Engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(args[1], args[2], args[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(Engine)
+
+    Session = sessionmaker(bind=Engine)
+    session = Session()
+
+    tup_st = session.query(State).outerjoin(City).order_by(City.id).all()
+
+    for states in tup_st:
+        for cities in states.cities:
+            print("{}: {} -> {}".format(cities.id, cities.name, states.name))
