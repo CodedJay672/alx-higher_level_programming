9634847e11f85869e78ba8e614741544242094b0
codedjay672 <emmajoel672@gmail.com>
2022-12-23 03:44:46 +0000

    update select first ORM

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""
+    script which fetches the first state from a database
+    script args:
+        @host: localhost
+        @u.name: root
+        @passwd: root
+
+"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+    rows = session.query(State).order_by(State.id).first()
+    if rows:
+        print('{}: {}'.format(rows.id, rows.name))
