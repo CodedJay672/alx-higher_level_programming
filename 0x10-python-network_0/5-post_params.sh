07802344473f8f74f8b75e05eb23480afa15f554
codedjay672 <emmajoel672@gmail.com>
2023-01-16 19:37:46 +0000

    update file

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that sends a POST request with some pameters
-curl -sX POST -d "email=test@gmail.com&subject=I will always be here for PLS" "$1"
+curl -sX POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
