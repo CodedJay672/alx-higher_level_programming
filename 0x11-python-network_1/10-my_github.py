161c5006a0d01ec723eb18fb78abb266ed99b27c
codedjay672 <emmajoel672@gmail.com>
2023-01-18 18:11:49 +0000

    update github file

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -7,7 +7,7 @@ script takes a username and password as arguments
 
 import sys
 import requests
-from from requests.auth import HTTPBasicAuth
+from requests.auth import HTTPBasicAuth
 
 if __name__ == "__main__":
     login = HTTPBasicAuth(sys.argv[1], sys.argv[2])
