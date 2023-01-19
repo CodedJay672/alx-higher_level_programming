61bccd769898e54b29552def4887b78a205ba0b9
codedjay672 <emmajoel672@gmail.com>
2023-01-19 05:49:28 +0000

    catch me

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that makes a request to a server, which in turn, responds in a certain way
-curl -sLX PUT  0.0.0.0:5000/catch_me
+curl -sLX PUT -d "user_id=98" 0.0.0.0:5000/catch_me
