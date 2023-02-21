d6145f2669509af75eacf73d039c98353f926dda
codedjay672 <emmajoel6456@gmail.com>
2023-02-21 19:07:56 +0100

    add files

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,8 @@
+$("DIV#toggle_header").click(function () {
+  $(this).toggleClass(function () {
+          if ($(this).hasClass("red"))
+                  return "green";
+          else
+                  return "red";
+  });
+});
